c478b81595f519145767f71aee99b4433289e8cf
Marco Mac <mclarenmarco998@gmail.com>
2023-02-01 03:09:06 +0200

    real time income and expenses calculator

--- src/app/budget/budget.component.ts
+++ src/app/budget/budget.component.ts
@@ -2,68 +2,50 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-budget',
+  templateUrl: './budget.component.html',
   styleUrls: ['./budget.component.css'],
-
-  template: `<form (ngSubmit)="addIncome()">
-  <input type="text" [(ngModel)]="incomeDescription" placeholder="Description">
-  <input type="number" [(ngModel)]="incomeAmount" placeholder="Amount">
-  <button type="submit">Add Income</button>
-</form>
-<form (ngSubmit)="addExpense()">
-  <input type="text" [(ngModel)]="expenseDescription" placeholder="Description">
-  <input type="number" [(ngModel)]="expenseAmount" placeholder="Amount">
-  <button type="submit">Add Expense</button>
-</form>
-<h2>Income</h2>
-<ul>
-  <li *ngFor="let income of incomes">{{ incomeDescription }}: {{ incomeAmount }}</li>
-</ul>
-<h2>Expenses</h2>
-<ul>
-  <li *ngFor="let expense of expenses">{{ expenseDescription }}: {{ expenseAmount }}</li>
-</ul>
-<button (click)="calculateBalance()">Calculate</button>
-<p *ngIf="showBalance">Balance: {{ balance }}</p>`
 })
 
 
 export class BudgetComponent {
+ income = 0;
+ expenses = 0;
 
-  incomes: { description: string; amount: number; }[] = [];
-  incomeDescription = '';
-  incomeAmount = 0;
+  incomeArray = [];
+  expensesArray = [];
+ 
 
-  expenses: { description: string; amount: number; }[] = [];
-  expenseDescription = '';
-  expenseAmount = 0;
+//   expenses: { description: string; amount: number; }[] = [];
+//   expenseDescription = '';
+//   expenseAmount = 0;
 
-  balance = 0;
-  showBalance = false;
+//   balance = 0;
+//   showBalance = false;
 
-  addIncome() {
-    this.incomes.push({ description: this.incomeDescription, amount: this.incomeAmount });
-    this.incomeDescription = '';
-    this.incomeAmount = 0;
-    console.log(this.incomes)
-  }
+//   addIncome() {
+//     this.incomes.push({ description: this.incomeDescription, amount: this.incomeAmount });
+//     this.incomeDescription = '';
+//     this.incomeAmount = 0;
+//     console.log(this.incomes)
+//   }
 
-  addExpense() {
-    this.expenses.push({ description: this.expenseDescription, amount: this.expenseAmount });
-    this.expenseDescription = '';
-    this.expenses;
-    console.log(this.expenses)
-}
+//   addExpense() {
+//     this.expenses.push({ description: this.expenseDescription, amount: this.expenseAmount });
+//     this.expenseDescription = '';
+//     this.expenses;
+//     console.log(this.expenses)
+// }
 
-calculateBalance(){
-  let totalIncome = 0;
-  let totalExpense = 0;
-  for (let income of this.incomes) {
-    totalIncome += income.amount;
-  }
-  for (let expense of this.expenses) {
-    totalExpense += expense.amount;
-  }
-  this.balance = totalIncome - totalExpense;
-  this.showBalance = true;
-}
+// calculateBalance(){
+//   let totalIncome = 0;
+//   let totalExpense = 0;
+//   for (let income of this.incomes) {
+//     totalIncome += income.amount;
+//   }
+//   for (let expense of this.expenses) {
+//     totalExpense += expense.amount;
+//   }
+//   this.balance = totalIncome - totalExpense;
+//   this.showBalance = true;
+// }
 }
